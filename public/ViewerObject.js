dfa40b493f0ae51c204830263b11df698db78879
b-gr <reunion.canons.9l@icloud.com>
2026-06-05 20:19:34 +0100

    Added bin (closed and open) to drag to remove gates from the page

--- public/ViewerObject.js
+++ public/ViewerObject.js
@@ -1,9 +1,9 @@
 export default class ViewerObject {
-    constructor(type) {
+    constructor(type, state) {
         this.coordinates = { x: 0, y: 0 };
         this.type = type;
-        this.html = this.createHTML();
-        this.state = false;
+        this.html = this.createHTML(state);
+        this.state = state;
     }
 
     createHTML(newState) {
@@ -17,51 +17,184 @@ export default class ViewerObject {
         return container;
     }
 
-     
-    startSVGtrue = () => {
-        return 
-        `<svg 
-            width="50" 
-            height="50" 
-            fill="blue"
-            viewBox="0 0 50 50" 
+    binOpenSVG = () => {
+        return `
+        <svg 
+            viewBox="0 0 100 100" 
             xmlns="http://www.w3.org/2000/svg">
-            <circle 
-                r="25" cx="25" cy="25">
-            </circle>
-            <text 
-                font-size="45" 
-                fill="white"
-                font-family="serif"
-                x="50%" y="80%" 
-                text-anchor="middle"
-                dominant-baseline="inherit">
-                    1
-            </text>
-        </svg>`;
+            fill="white"
+            <path 
+                d="
+                    M22.5 30
+                    L77.5 30
+                    L72.5 90
+                    L27.5 90
+                    Z
+
+                    M25 10
+                    L80 30
+                    L80 23
+                    L32 5
+                    Z        
+                " 
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"/>
+            <path 
+                d="
+                M35 40
+                L37.5 80
+                M50 40
+                L50 80
+                M65 40
+                L62.5 80
+                "
+            stroke="black"
+            stroke-width="5"
+            stroke-linecap="round"/>
+        </svg>
+        `;
+    }
+
+
+
+    binclosedSVG = () => {
+        return `
+        <svg 
+            viewBox="0 0 100 100" 
+            xmlns="http://www.w3.org/2000/svg">
+            fill="white"
+            <path 
+                d="
+                    M22.5 30
+                    L77.5 30
+                    L72.5 90
+                    L27.5 90
+                    Z
+
+                    M20 30
+                    L80 30
+                    L75 25
+                    L25 25
+                    Z        
+                " 
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"/>
+            <path 
+                d="
+                M35 40
+                L37.5 80
+                M50 40
+                L50 80
+                M65 40
+                L62.5 80
+                "
+            stroke="black"
+            stroke-width="5"
+            stroke-linecap="round"/>
+        </svg>
+        `;
+    }
+
+    //endTrue
+    endSVGtrue = () => {
+        return `
+            <svg 
+                width="50" 
+                height="50" 
+                fill="blue"
+                viewBox="0 0 50 50" 
+                xmlns="http://www.w3.org/2000/svg">
+                <rect 
+                    width="50" 
+                    height="50">
+                </rect>
+                <text 
+                    font-size="45" 
+                    fill="white"
+                    font-family="serif"
+                    x="50%" y="80%" 
+                    text-anchor="middle"
+                    dominant-baseline="inherit">
+                        1
+                </text>
+            </svg>
+        `;
+    }
+
+    endSVGfalse = () => {
+        return `
+            <svg 
+                width="50" 
+                height="50" 
+                fill="red"
+                viewBox="0 0 50 50" 
+                xmlns="http://www.w3.org/2000/svg">
+                <rect 
+                    width="50" 
+                    height="50">
+                </rect>
+                <text 
+                    font-size="45" 
+                    fill="white"
+                    font-family="serif"
+                    x="50%" y="80%" 
+                    text-anchor="middle"
+                    dominant-baseline="inherit">
+                        0
+                </text>
+            </svg>
+        `;
+    }
+
+
+    startSVGtrue = () => {
+        return `
+            <svg 
+                width="50" 
+                height="50" 
+                fill="blue"
+                viewBox="0 0 50 50" 
+                xmlns="http://www.w3.org/2000/svg">
+                <circle 
+                    r="25" cx="25" cy="25">
+                </circle>
+                <text 
+                    font-size="45" 
+                    fill="white"
+                    font-family="serif"
+                    x="50%" y="80%" 
+                    text-anchor="middle"
+                    dominant-baseline="inherit">
+                        1
+                </text>
+            </svg>
+        `;
     }
 
     startSVGfalse = () => {
-        return 
-        `<svg 
-            width="50" 
-            height="50" 
-            fill="red"
-            viewBox="0 0 50 50" 
-            xmlns="http://www.w3.org/2000/svg">
-            <circle 
-                r="25" cx="25" cy="25">
-            </circle>
-            <text 
-                font-size="45" 
-                fill="white"
-                font-family="serif"
-                x="50%" y="80%" 
-                text-anchor="middle"
-                dominant-baseline="inherit">
-                    0
-            </text>
-        </svg>`;
+        return `
+            <svg 
+                width="50" 
+                height="50" 
+                fill="red"
+                viewBox="0 0 50 50" 
+                xmlns="http://www.w3.org/2000/svg">
+                <circle 
+                    r="25" cx="25" cy="25">
+                </circle>
+                <text 
+                    font-size="45" 
+                    fill="white"
+                    font-family="serif"
+                    x="50%" y="80%" 
+                    text-anchor="middle"
+                    dominant-baseline="inherit">
+                        0
+                </text>
+            </svg>
+        `;
     }
 
 
