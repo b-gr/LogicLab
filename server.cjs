10f1934ef99705c079e625aca92e9d76593b3f64
b-gr <reunion.canons.9l@icloud.com>
2026-09-03 12:33:42 +1000

    Updated to improve testing

--- server.cjs
+++ server.cjs
@@ -5,12 +5,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-*/
-
 
 app.listen(port, () => {
     console.log(`LogicLab running at http://localhost:${port}`);
--- tests/LogicGate.test.js
+++ tests/LogicGate.test.js
@@ -7,6 +7,27 @@ test("AND gate: true AND true", () => {
     gate.type = "AND";
     gate.getInputValues = () => [true,true];
     const result = gate.booleanOperation();
-
     assert.equal(result, true)
+})
+
+test("AND gate: true AND false", () => {
+    const gate = new LogicGate('AND');
+    gate.getInputValues = () => [true,false];
+    const result = gate.booleanOperation();
+    assert.equal(result, false)
+})
+
+
+test("AND gate: false AND false", () => {
+    const gate = new LogicGate('AND');
+    gate.getInputValues = () => [false,false];
+    const result = gate.booleanOperation();
+    assert.equal(result, false)
+})
+
+test("AND gate: false AND true", () => {
+    const gate = new LogicGate('AND');
+    gate.getInputValues = () => [false,true];
+    const result = gate.booleanOperation();
+    assert.equal(result, false)
 })
