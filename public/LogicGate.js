7e6c834dcc0f4c2f8b5bc9c1b5264be91ef69c7e
b-gr <reunion.canons.9l@icloud.com>
2026-09-04 22:52:49 +1000

    formating and turning off admin mode

--- public/LogicGate.js
+++ public/LogicGate.js
@@ -157,214 +157,225 @@ export default class LogicGate {
 
     nandSVG = () => {
         return `
-        <svg
-            width="${this.size.width}" 
-            height="${this.size.height}" 
-                viewBox="0 0 110 60" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
-    <path 
-        d="
-            M90 30 
-            C90 15 80 0.5 60 0.5 
-            L6 0.5 
-            V59.5 
-            H59.5 
-            C80 60 90 50 90 30 
-            Z 
+            <svg
+                width="${this.size.width}" 
+                height="${this.size.height}" 
+                    viewBox="0 0 110 60" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg">
+                <path 
+                    d="
+                        M90 30 
+                        C90 15 80 0.5 60 0.5 
+                        L6 0.5 
+                        V59.5 
+                        H59.5 
+                        C80 60 90 50 90 30 
+                        Z 
 
-            M96 30 
-            H110
+                        M96 30 
+                        H110
 
-            M0 15 
-            H6
+                        M0 15 
+                        H6
 
-            M0 45 
-            H6
-        " 
-        stroke="white" 
-        stroke-linecap="round"/>
-    <circle 
-        cx="93" 
-        cy="30" 
-        r="3" 
-        stroke="white" 
-        stroke-linecap="round"
-        />
-</svg>`;
+                        M0 45 
+                        H6
+                    " 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+                <circle 
+                    cx="93" 
+                    cy="30" 
+                    r="3" 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+            </svg>
+        `;
     }
 
     orSVG = () => {
         return `
-<svg
-            width="${this.size.width}" 
-            height="${this.size.height}" 
-                viewBox="0 0 110 60" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
+            <svg
+                width="${this.size.width}" 
+                height="${this.size.height}" 
+                    viewBox="0 0 110 60" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg">
                 <path 
-        d="
-            M6 0.5 
-            C64 2 82 10 90 30
-            C82 50 64 58 6 59.5
-            C25 40 25 19.5 6 0.5
-            Z
+                    d="
+                        M6 0.5 
+                        C64 2 82 10 90 30
+                        C82 50 64 58 6 59.5
+                        C25 40 25 19.5 6 0.5
+                        Z
 
-            M90 30 
-            H110
+                        M90 30 
+                        H110
 
-            M0 15 
-            H16
+                        M0 15 
+                        H16
 
-            M0 45 
-            H16
-        " 
-        stroke="white" 
-        stroke-linecap="round"/>
-</svg>`;
+                        M0 45 
+                        H16
+                    " 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+            </svg>
+        `;
     }
 
     norSVG = () => {
         return `
-<svg
-            width="${this.size.width}" 
-            height="${this.size.height}" 
-                viewBox="0 0 110 60" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
+            <svg
+                width="${this.size.width}" 
+                height="${this.size.height}" 
+                    viewBox="0 0 110 60" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg">
                 <path 
-        d="
-            M6 0.5 
-            C64 2 82 10 90 30
-            C82 50 64 58 6 59.5
-            C25 40 25 19.5 6 0.5
-            Z
+                    d="
+                        M6 0.5 
+                        C64 2 82 10 90 30
+                        C82 50 64 58 6 59.5
+                        C25 40 25 19.5 6 0.5
+                        Z
 
-            M96.5 30 
-            H110
+                        M96.5 30 
+                        H110
 
-            M0 15 
-            H16
+                        M0 15 
+                        H16
 
-            M0 45 
-            H16
-        " 
-        stroke="white" 
-        stroke-linecap="round"/>
-    <circle 
-        cx="93.5" 
-        cy="30" 
-        r="3" 
-        stroke="white" 
-        stroke-linecap="round"
-        />
-</svg>`;
+                        M0 45 
+                        H16
+                    " 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+                <circle 
+                    cx="93.5" 
+                    cy="30" 
+                    r="3" 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+            </svg>
+        `;
     }
 
     notSVG = () => {
         return `
-<svg
-            width="${this.size.width}" 
-            height="${this.size.height}" 
-                viewBox="0 0 110 60" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
+            <svg
+                width="${this.size.width}" 
+                height="${this.size.height}" 
+                    viewBox="0 0 110 60" 
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg">
                 <path 
-        d="
-            M6 0.5 
-            L57 30
-            L6 59.5
-            Z
+                    d="
+                        M6 0.5 
+                        L57 30
+                        L6 59.5
+                        Z
 
-            M63.5 30 
-            H110
+                        M63.5 30 
+                        H110
 
-            M0 30 
-            H6
-        " 
-        stroke="white" 
-        stroke-linecap="round"/>
-    <circle 
-        cx="60.5" 
-        cy="30" 
-        r="3" 
-        stroke="white" 
-        stroke-linecap="round"
-        />
-</svg>`;
+                        M0 30 
+                        H6
+                    " 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+                <circle 
+                    cx="60.5" 
+                    cy="30" 
+                    r="3" 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+            </svg>
+        `;
     }
 
     xnorSVG = () => {
         return `
-<svg
-            width="${this.size.width}" 
-            height="${this.size.height}" 
+            <svg
+                width="${this.size.width}" 
+                height="${this.size.height}" 
                 viewBox="0 0 110 60" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
+                fill="none" 
+                xmlns="http://www.w3.org/2000/svg">
                 <path 
-        d="
-            M6 0.5 
-            C64 2 82 10 90 30
-            C82 50 64 58 6 59.5
-            C25 40 25 19.5 6 0.5
-            Z
+                    d="
+                        M6 0.5 
+                        C64 2 82 10 90 30
+                        C82 50 64 58 6 59.5
+                        C25 40 25 19.5 6 0.5
+                        Z
 
-            M0 59.5 
-            C19 40 19 19.5 0 0.5
+                        M0 59.5 
+                        C19 40 19 19.5 0 0.5
 
-            M96.5 30 
-            H110
+                        M96.5 30 
+                        H110
 
-            M0 15 
-            H16
+                        M0 15 
+                        H16
 
-            M0 45 
-            H16
-        " 
-        stroke="white" 
-        stroke-linecap="round"/>
-    <circle 
-        cx="93.5" 
-        cy="30" 
-        r="3" 
-        stroke="white" 
-        stroke-linecap="round"
-        />
-        </svg>`;
+                        M0 45 
+                        H16
+                    " 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+                <circle 
+                    cx="93.5" 
+                    cy="30" 
+                    r="3" 
+                    stroke="white" 
+                    stroke-linecap="round"
+                />
+            </svg>
+        `;
     }
 
     xorSVG = () => {
         return `
-<svg
-            width="${this.size.width}" 
-            height="${this.size.height}" 
-                viewBox="0 0 110 60" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg">
-                <path 
-        d="
-            M6 0.5 
-            C64 2 82 10 90 30
-            C82 50 64 58 6 59.5
-            C25 40 25 19.5 6 0.5
-            Z
+            <svg
+                    width="${this.size.width}" 
+                    height="${this.size.height}" 
+                        viewBox="0 0 110 60" 
+                    fill="none" 
+                    xmlns="http://www.w3.org/2000/svg">
+                        <path 
+                d="
+                    M6 0.5 
+                    C64 2 82 10 90 30
+                    C82 50 64 58 6 59.5
+                    C25 40 25 19.5 6 0.5
+                    Z
 
-            M0 59.5 
-            C19 40 19 19.5 0 0.5
+                    M0 59.5 
+                    C19 40 19 19.5 0 0.5
 
-            M90 30 
-            H110
+                    M90 30 
+                    H110
 
-            M0 15 
-            H16
+                    M0 15 
+                    H16
 
-            M0 45 
-            H16
-        " 
-        stroke="white" 
-        stroke-linecap="round"/>
-</svg>`;
+                    M0 45 
+                    H16
+                " 
+                stroke="white" 
+                stroke-linecap="round"/>
+            </svg>
+        `;
     }
 }
 
