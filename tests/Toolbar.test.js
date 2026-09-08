aa77239529962c76ce6ab9e5b4bb20afaa2f1570
b-gr <reunion.canons.9l@icloud.com>
2026-09-08 12:41:16 +1000

    Installed dependancy JSDOM

---
+++ tests/Toolbar.test.js
@@ -0,0 +1,8 @@
+import test from "node:test";
+import assert from "node:assert";
+import {JSDOM} from "jsdom"
+import Toolbar from "../public/Toolbar.js";
+
+test("test2", () => {
+    assert.equal(1,1)
+})
